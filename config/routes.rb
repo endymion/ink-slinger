4aaafa495dfb94d0c79458bb7a23b8e587ddf23e
Ryan Alyn Porter <rap@endymion.com>
2010-06-25 16:39:27 -0400

    Added a basic home controller.

--- config/routes.rb
+++ config/routes.rb
@@ -46,9 +46,7 @@ Reporter::Application.routes.draw do |map|
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
