b18b22f32fc34ce9608a6463837910165b4b7aeb
Ryan Alyn Porter <rap@endymion.com>
2010-07-02 13:06:27 -0400

    Stop Compass from auto compiling SASS in production.

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -3,3 +3,6 @@ rails_root = (defined?(Rails) ? Rails.root : RAILS_ROOT).to_s
 Compass.add_project_configuration(File.join(rails_root, "config", "compass.rb"))
 Compass.configure_sass_plugin!
 Compass.handle_configuration_change!
+
+# Manually compile SASS, don't auto update.
+Sass::Plugin.options[:never_update]=true
