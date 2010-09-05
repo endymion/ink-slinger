52f0084f7773b7a03cb520bfe6414a2032387981
Ryan Alyn Porter <rap@endymion.com>
2010-09-05 14:20:30 -0400

    Added tests for the TopicHelper methods.

--- spec/helpers/topics_helper_spec.rb
+++ spec/helpers/topics_helper_spec.rb
@@ -11,5 +11,46 @@ require 'spec_helper'
 #   end
 # end
 describe TopicsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "new_image should create a new Image for a Topic" do
+    topic = Topic.new
+    topic.images.size.should == 0
+    helper.new_image(topic)
+    topic.images.size.should == 1
+  end
+  
+  it "new_panel should create a new Panel for a Topic" do
+    topic = Topic.new
+    topic.panels.size.should == 0
+    helper.new_panel(topic)
+    topic.panels.size.should == 1    
+  end
+
+  it "new_panel should not create a new Panel for a Topic if one already exists" do
+    topic = Topic.new
+    helper.new_panel(topic)
+    topic.panels.size.should == 1    
+    helper.new_panel(topic)
+    topic.panels.size.should == 1    
+  end
+
+  it "id_or_new_count should return '-[id]' when given an object" do
+    topic = double('Topic')
+    topic.stub(:id).and_return(37)
+    helper.id_or_new_count(topic).should == '-37'
+  end
+
+  it "id_or_new_count should return '-new-1' when given a new object" do
+    topic = double('Topic')
+    topic.stub(:id).and_return(nil)
+    helper.id_or_new_count(topic).should == '-new-1'
+  end
+
+  it "id_or_new_count should increment the '-new-?' count when given another new object" do
+    topic = double('Topic')
+    topic.stub(:id).and_return(nil)
+    helper.id_or_new_count(topic).should == '-new-1'
+    helper.id_or_new_count(topic).should == '-new-2'
+  end
+
 end
