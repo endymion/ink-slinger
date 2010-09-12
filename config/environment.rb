9ac0390cc25a28c0907a37dbf6687f2ffd955c6f
Ryan Alyn Porter <rap@endymion.com>
2010-09-12 19:23:47 -0400

    Heroku allows writing to ./tmp, not /tmp.

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Reporter::Application.initialize!
 
-ActionController::Base.page_cache_directory = "/tmp"
+ActionController::Base.page_cache_directory = "./tmp" # For Heroku.
