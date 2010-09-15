9c59b87c2aaf78962403c0ea2642b8688ac19dda
Ryan Alyn Porter <rap@endymion.com>
2010-09-15 16:10:44 -0400

    Finished live site spec with plain RSpec, no Cucumber or Steak.

--- spec/live_sites_spec.rb
+++ spec/live_sites_spec.rb
@@ -11,15 +11,15 @@ end
 # Use Selenium, to test real live sites with a real web browser.
 Capybara.current_driver = :selenium
 
-# Use the real config/brands.yml, not the testing configuration.
+# Use the real config/brands.yml, not the testing configuration from spec_helper.
 DatabaseCleaner.clean_with :truncation
 Brand.seed
 
-describe "live servers" do
+describe "Live" do
 
   describe "asset hosts" do
-    @urls = Brand.asset_hosts.map { |host| 'http://' + host }
-    @urls.each do |url|
+    urls = Brand.asset_hosts.map { |host| 'http://' + host }
+    urls.each do |url|
       it url do
         Capybara.app_host = url
         visit('/test.html')
@@ -28,28 +28,29 @@ describe "live servers" do
     end
   end
   
-  # describe "application hosts" do
-  #   @urls = Brand.application_hosts.map { |host| 'http://' + host }
-  #   @urls.each do |url|
-  #     it url do
-  #       host = url
-  #       host = 
-  #       Capybara.app_host = url
-  #       visit('/')
-  #       page.should have_css '#page'
-  #     end
-  #   end
-  # end
-  # 
-  # describe "application caches" do
-  #   @urls = Brand.application_hosts.map { |host| 'http://' + host }
-  #   @urls.each do |url|
-  #     it url do
-  #       Capybara.app_host = url
-  #       visit('/')
-  #       page.should have_css '#page'
-  #     end
-  #   end
-  # end
+  describe "application hosts" do
+    brands = Brand.all
+    brands.each do |brand|
+      url = brand.application_host
+      it url do
+        Capybara.app_host = url
+        visit('/')
+        page.should have_css '#page'
+      end
+    end
+  end
+  
+  describe "application caches" do
+    brands = Brand.all
+    brands.each do |brand|
+      url = brand.application_cache_host
+      next if url.blank?
+      it url do
+        Capybara.app_host = url
+        visit('/')
+        page.should have_css '#page'
+      end
+    end
+  end
   
 end
