956bf6823f625ef78732a67bc33f1441b52f2479
Ryan Alyn Porter <rap@endymion.com>
2010-09-18 09:58:55 -0400

    Fixed asset host URL rewriting problem for production mode.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 module ApplicationHelper
   
   def brand_asset_url(url)
+    brand_asset_host = (Thread.current[:brand].asset_host.gsub /\%d/, rand(4).to_s)
+    if url.match /#{PAPERCLIP_CONFIG[:bucket]}(\/.*)$/
+      return 'http://' + brand_asset_host + $1
+    end
     url.gsub /\Ahttp\:\/\/[^\/]+\//,
-      ((Thread.current[:brand].asset_host.gsub /\%d/, rand(4).to_s) + '/')
+      (brand_asset_host + '/')
   end
   
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
+
+  PAPERCLIP_CONFIG = { :bucket => "static.brave-new-media.com" }
   
   it "brand_asset_url(url) generates an asset server URL for the current brand" do
     Thread.current[:brand] = Brand.where(
       :subdomain => 'miami', :domain_name => 'something-chronicle.com').first
     url = helper.brand_asset_url('http://something.com/something')
     url.should match /miami\-\d\.some\-keyword\-events\.com\/something/
+  end
+
+  it "brand_asset_url(url) generates an asset server URL from an S3 URL" do
+    Thread.current[:brand] = Brand.where(
+      :subdomain => 'miami', :domain_name => 'something-chronicle.com').first
+    url = helper.brand_asset_url('http://s3.amazonaws.com/static.brave-new-media.com/system/panels/tile_256s/1/original.jpg?1284771276')
+    url.should match /miami\-\d\.some\-keyword\-events\.com\/system\/panels/
   end
   
 end
