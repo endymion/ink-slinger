465b867a0372a23fbfcd96ffeca1175ee21501cd
Ryan Alyn Porter <rap@endymion.com>
2010-09-18 17:50:47 -0400

    Don't rewrite brand asset URLs in development mode.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   
   def brand_asset_url(url)
+    return url if Rails.env.eql? 'development'
     brand_asset_host = (Thread.current[:brand].asset_host.gsub /\%d/, rand(4).to_s)
     if url.match /#{PAPERCLIP_CONFIG[:bucket]}(\/.*)$/
       return 'http://' + brand_asset_host + $1
