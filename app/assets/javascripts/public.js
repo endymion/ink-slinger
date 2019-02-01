8297a72ad04d55dfdd6b11c661a2cecc9926bbe6
Ryan Porter <rap@endymion.com>
2019-01-31 21:22:48 -0500

    GrapeJS - Getting Started - Panels & Buttons
    https://grapesjs.com/docs/getting-started.html#panels-buttons

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -45,4 +45,40 @@ window.onload = function() {
     },
   });
 
+  editor.Panels.addPanel({
+    id: 'panel-top',
+    el: '.panel__top',
+  });
+  editor.Panels.addPanel({
+    id: 'basic-actions',
+    el: '.panel__basic-actions',
+    buttons: [
+      {
+        id: 'visibility',
+        active: true, // active by default
+        className: 'btn-toggle-borders',
+        label: '<u>B</u>',
+        command: 'sw-visibility', // Built-in command
+      }, {
+        id: 'export',
+        className: 'btn-open-export',
+        label: 'Exp',
+        command: 'export-template',
+        context: 'export-template', // For grouping context of buttons from the same panel
+      }, {
+        id: 'show-json',
+        className: 'btn-show-json',
+        label: 'JSON',
+        context: 'show-json',
+        command(editor) {
+          editor.Modal.setTitle('Components JSON')
+            .setContent(`<textarea style="width:100%; height: 250px;">
+              ${JSON.stringify(editor.getComponents())}
+            </textarea>`)
+            .open();
+        },
+      }
+    ],
+  });
+  
 };
