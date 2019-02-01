aabf4d6836672d41ae48819037f608b932fc51c0
Ryan Porter <rap@endymion.com>
2019-01-31 21:29:15 -0500

    GrapeJS - Getting Started - Layers
    https://grapesjs.com/docs/getting-started.html#layers

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -43,6 +43,30 @@ window.onload = function() {
         }
       ]
     },
+
+    // ...
+    layerManager: {
+      appendTo: '.layers-container'
+    },
+    // We define a default panel as a sidebar to contain layers
+    panels: {
+      defaults: [{
+        id: 'layers',
+        el: '.panel__right',
+        // Make the panel resizable
+        resizable: {
+          maxDim: 350,
+          minDim: 200,
+          tc: 0, // Top handler
+          cl: 1, // Left handler
+          cr: 0, // Right handler
+          bc: 0, // Bottom handler
+          // Being a flex child we need to change `flex-basis` property
+          // instead of the `width` (default)
+          keyWidth: 'flex-basis',
+        },
+      }]
+    }
   });
 
   editor.Panels.addPanel({
@@ -80,5 +104,5 @@ window.onload = function() {
       }
     ],
   });
-  
+
 };
