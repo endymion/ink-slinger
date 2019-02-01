d2c573e6673f6200381eca803f96704cbf6b749e
Ryan Porter <rap@endymion.com>
2019-01-31 21:53:23 -0500

    GrapesJS - Getting Started - Style Manager
    https://grapesjs.com/docs/getting-started.html#style-manager

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -48,25 +48,106 @@ window.onload = function() {
     layerManager: {
       appendTo: '.layers-container'
     },
-    // We define a default panel as a sidebar to contain layers
+    // ...
     panels: {
-      defaults: [{
-        id: 'layers',
-        el: '.panel__right',
-        // Make the panel resizable
-        resizable: {
-          maxDim: 350,
-          minDim: 200,
-          tc: 0, // Top handler
-          cl: 1, // Left handler
-          cr: 0, // Right handler
-          bc: 0, // Bottom handler
-          // Being a flex child we need to change `flex-basis` property
-          // instead of the `width` (default)
-          keyWidth: 'flex-basis',
-        },
-      }]
-    }
+      defaults: [
+        // ...
+        {
+          id: 'panel-switcher',
+          el: '.panel__switcher',
+          buttons: [{
+              id: 'show-layers',
+              active: true,
+              label: 'Layers',
+              command: 'show-layers',
+              // Once activated disable the possibility to turn it off
+              togglable: false,
+            }, {
+              id: 'show-style',
+              active: true,
+              label: 'Styles',
+              command: 'show-styles',
+              togglable: false,
+          }],
+        }
+      ]
+    },
+    // The Selector Manager allows to assign classes and
+    // different states (eg. :hover) on components.
+    // Generally, it's used in conjunction with Style Manager
+    // but it's not mandatory
+    selectorManager: {
+      appendTo: '.styles-container'
+    },
+    styleManager: {
+      appendTo: '.styles-container',
+      sectors: [{
+          name: 'Dimension',
+          open: false,
+          // Use built-in properties
+          buildProps: ['width', 'min-height', 'padding'],
+          // Use `properties` to define/override single property
+          properties: [
+            {
+              // Type of the input,
+              // options: integer | radio | select | color | slider | file | composite | stack
+              type: 'integer',
+              name: 'The width', // Label for the property
+              property: 'width', // CSS property (if buildProps contains it will be extended)
+              units: ['px', '%'], // Units, available only for 'integer' types
+              defaults: 'auto', // Default value
+              min: 0, // Min value, available only for 'integer' types
+            }
+          ]
+        },{
+          name: 'Extra',
+          open: false,
+          buildProps: ['background-color', 'box-shadow', 'custom-prop'],
+          properties: [
+            {
+              id: 'custom-prop',
+              name: 'Custom Label',
+              property: 'font-size',
+              type: 'select',
+              defaults: '32px',
+              // List of options, available only for 'select' and 'radio'  types
+              options: [
+                { value: '12px', name: 'Tiny' },
+                { value: '18px', name: 'Medium' },
+                { value: '32px', name: 'Big' },
+              ],
+           }
+          ]
+        }]
+    },
+  });
+
+  // Define commands
+  editor.Commands.add('show-layers', {
+    getRowEl(editor) { return editor.getContainer().closest('.editor-row'); },
+    getLayersEl(row) { return row.querySelector('.layers-container') },
+
+    run(editor, sender) {
+      const lmEl = this.getLayersEl(this.getRowEl(editor));
+      lmEl.style.display = '';
+    },
+    stop(editor, sender) {
+      const lmEl = this.getLayersEl(this.getRowEl(editor));
+      lmEl.style.display = 'none';
+    },
+  });
+  editor.Commands.add('show-styles', {
+    getRowEl(editor) { return editor.getContainer().closest('.editor-row'); },
+    getStyleEl(row) { return row.querySelector('.styles-container') },
+
+    run(editor, sender) {
+      const smEl = this.getStyleEl(this.getRowEl(editor));
+      smEl.style.display = '';
+    },
+    stop(editor, sender) {
+      const smEl = this.getStyleEl(this.getRowEl(editor));
+      smEl.style.display = 'none';
+    },
   });
 
   editor.Panels.addPanel({
